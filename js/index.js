ae384a1c86e134535adebc37c126af37d744a900
Anderson Fernandes <cabula21@yahoo.com.br>
2023-10-19 18:49:55 -0300

    EPARK-Tela Login

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 if (localStorage.getItem("token") == null) {
-  alert("Você precisa estar logado para acessar essa página");
-  window.location.href = "./assets/html/singin.html";
+  window.location.href = "./html/signin.html";
 }
 
 const userLogado = JSON.parse(localStorage.getItem("userLogado"));
@@ -11,5 +10,5 @@ logado.innerHTML = `Olá ${userLogado.nome}`;
 function sair() {
   localStorage.removeItem("token");
   localStorage.removeItem("userLogado");
-  window.location.href = "./assets/html/signin.html";
+  window.location.href = "./html/signin.html";
 }
