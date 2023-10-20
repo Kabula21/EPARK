7a943be11cef73ea96b850aff0aca10c483d579e
Anderson Fernandes <cabula21@yahoo.com.br>
2023-10-20 18:59:37 -0300

    EPARK - Tela Login e Tela Dashboard

--- js/signin.js
+++ js/signin.js
@@ -1,63 +1,45 @@
-let btn = document.querySelector('.fa-eye')
+function logar(){
 
-btn.addEventListener('click', ()=>{
-  let inputSenha = document.querySelector('#senha')
-  
-  if(inputSenha.getAttribute('type') == 'password'){
-    inputSenha.setAttribute('type', 'text')
-  } else {
-    inputSenha.setAttribute('type', 'password')
-  }
-})
+  var login = document.getElementById('login').value;
+  var senha = document.getElementById('senha').value;
 
-function entrar(){
-  let usuario = document.querySelector('#usuario')
-  let userLabel = document.querySelector('#userLabel')
-  
-  let senha = document.querySelector('#senha')
-  let senhaLabel = document.querySelector('#senhaLabel')
-  
-  let msgError = document.querySelector('#msgError')
-  let listaUser = []
-  
-  let userValid = {
-    nome: '',
-    user: '',
-    senha: ''
+  if(login == "admin" && senha == "admin"){
+    alert('sucesso');
+    location.href ="../html/page2.html";
   }
-  
-  listaUser = JSON.parse(localStorage.getItem('listaUser'))
-  
-  listaUser.forEach((item) => {
-    if(usuario.value == item.userCad && senha.value == item.senhaCad){
-       
-      userValid = {
-         nome: item.nomeCad,
-         user: item.userCad,
-         senha: item.senhaCad
-       }
-      
-    }
-  })
-   
-  if(usuario.value == userValid.user && senha.value == userValid.senha){
-    window.location.href = '../../index.html'
-    
-    let mathRandom = Math.random().toString(16).substr(2)
-    let token = mathRandom + mathRandom
-    
-    localStorage.setItem('token', token)
-    localStorage.setItem('userLogado', JSON.stringify(userValid))
-  } else {
-    userLabel.setAttribute('style', 'color: red')
-    usuario.setAttribute('style', 'border-color: red')
-    senhaLabel.setAttribute('style', 'color: red')
-    senha.setAttribute('style', 'border-color: red')
-    msgError.setAttribute('style', 'display: block')
-    msgError.innerHTML = 'Usuário ou senha incorretos'
-    usuario.focus()
+  else{
+    alert('Usuário ou Senha Incorretos');
   }
-  
+
 }
 
 
+function startProgress() {
+  let progressBar = document.getElementById("progress-bar");
+  let width = 0;
+  const interval = 10; // Intervalo de atualização em milissegundos
+  
+  const updateProgress = () => {
+      if (width >= 100) {
+          clearInterval(progressInterval);
+      } else {
+          width++;
+          progressBar.style.width = width + "%";
+          progressBar.innerHTML = width + "%";
+      }
+  };
+  
+  const progressInterval = setInterval(updateProgress, interval);
+}
+
+
+
+function openModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
