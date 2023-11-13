5d9d1e995f3a8d360f7b2053ee86c20daf01952d
Anderson Fernandes <cabula21@yahoo.com.br>
2023-11-13 10:04:53 -0300

    EPARK - SOLUÇÃO ENTRADA OK

--- js/actions.js
+++ js/actions.js
@@ -64,22 +64,46 @@
 
     });
 
+    // AJUSTE DATA E HORA DO SISTEMA E MODAL//
+    function openModalentrada() {
+        // Antes de abrir o modal, atualize a data e hora
+        atualizarDataHora();
+
+        // Obtenha os valores do formulário
+        const placa = document.getElementById('placaEntrada').value;
+        const observacoes = document.getElementById('observacoesEntrada').value;
+
+        // Atualize os elementos no modal
+        document.getElementById('modalPlacaEntrada').textContent = `Placa: ${placa}`;
+        document.getElementById('modalDataHora').textContent = `Data e Hora: ${document.getElementById('dataHora').textContent}`;
+        document.getElementById('modalObservacoesEntrada').textContent = `Observações: ${observacoes}`;
+
+        // Abra o modal
+        document.getElementById('myModalentrada').style.display = 'block';
+    }
+
+    function closeModalentrada() {
+        // Feche o modal
+        document.getElementById('myModalentrada').style.display = 'none';
+    }
 
     function atualizarDataHora() {
         const elementoDataHora = document.getElementById('dataHora');
         const dataHoraAtual = new Date();
-    
+
         const dia = dataHoraAtual.getDate().toString().padStart(2, '0');
         const mes = (dataHoraAtual.getMonth() + 1).toString().padStart(2, '0');
         const ano = dataHoraAtual.getFullYear();
         const horas = dataHoraAtual.getHours().toString().padStart(2, '0');
         const minutos = dataHoraAtual.getMinutes().toString().padStart(2, '0');
         const segundos = dataHoraAtual.getSeconds().toString().padStart(2, '0');
-    
+
         const dataHoraFormatada = `${dia}/${mes}/${ano} ${horas}:${minutos}:${segundos}`;
         elementoDataHora.textContent = dataHoraFormatada;
-
     }
+
+
+//FUNÇÃO DE REGISTRO DE ENTRADA//
     
     document.getElementById('registrarEntrada').addEventListener('click', () => {
         if (vagasOcupadas < totalVagas) {
@@ -104,40 +128,37 @@
         }
     });
     
-    document.getElementById('registrarSaida').addEventListener('click', () => {
-        const placaSaida = document.getElementById('placaSaida').value;
-    
-        if (veiculosEstacionados[placaSaida]) {
-            const horaSaida = new Date().getTime();
-            const tempoEstacionado = (horaSaida - veiculosEstacionados[placaSaida].horaEntrada) / 3600000;
-            const valorEstacionamento = tempoEstacionado * 10;
-    
-            const horaEntradaFormatada = formatarDataHora(veiculosEstacionados[placaSaida].horaEntrada);
-            const horaSaidaFormatada = formatarDataHora(horaSaida);
 
-            //Exibir informações adicionais
-            const informacoesAdicionais = veiculosEstacionados[placaSaida].informacoesAdicionais;
-            
+//FUNÇÃO DE REGISTRO DE SAIDA//
 
-    
-            if (valorEstacionamento > 1) {
-                const resultado = `Placa: ${placaSaida}<br>Hora de Entrada: ${horaEntradaFormatada}<br>Hora de Saída: ${horaSaidaFormatada}<br>Tempo Estacionado (horas): ${tempoEstacionado.toFixed(2)}<br>Valor a pagar: R$ ${valorEstacionamento.toFixed(2)}`;
-                document.getElementById('resultado').innerHTML = resultado;
-                document.getElementById('informacoesAdicionais').textContent = `Informações Adicionais: ${informacoesAdicionais}`;
-            } else {
-                const resultado = `Placa: ${placaSaida}<br>Hora de Entrada: ${horaEntradaFormatada}<br>Hora de Saída: ${horaSaidaFormatada}<br>Tempo Estacionado (horas): ${tempoEstacionado.toFixed(2)}<br>Valor a pagar: R$ 0.00`;
-                document.getElementById('resultado').innerHTML = resultado;
-                document.getElementById('informacoesAdicionais').textContent = `Informações Adicionais: ${informacoesAdicionais}`;
-            }
-            
-            delete veiculosEstacionados[placaSaida];
-            vagasOcupadas--;
-            vagasLivres++;
-            saidas++;
-        } else {
-            alert('Veículo não encontrado no estacionamento.');
-        }
-    });
+    // Função para abrir o modal de saída
+function openModalsaida() {
+    document.getElementById('myModalsaida').style.display = 'block';
+  }
+  
+  // Função para fechar o modal de saída
+  function closeModalsaida() {
+    document.getElementById('myModalsaida').style.display = 'none';
+  }
+  
+  document.getElementById('registrarSaida').addEventListener('click', () => {
+    const placaSaida = document.getElementById('placaSaida').value;
+  
+    if (veiculosEstacionados[placaSaida]) {
+      // ... (código existente)
+  
+      // Chamar a função para abrir o modal de saída
+      openModalsaida();
+    } else {
+      alert('Veículo não encontrado no estacionamento.');
+    }
+  });
+  
+  // Função para formatar data e hora (substitua com a sua implementação)
+  function formatarDataHora(timestamp) {
+    // Implementação da formatação de data e hora
+  }
+  
     
      // Chama a função para atualizar a data e hora a cada segundo (1000 milissegundos ou 1 segundo)
      setInterval(atualizarDataHora, 1000);
